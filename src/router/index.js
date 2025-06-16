a7883dde38f1321e98d131ea601927db871689ab
juronja <101410098+juronja@users.noreply.github.com>
2025-06-16 16:15:40 +0200

    guided view commented out

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,14 @@ const router = createRouter({
       name: 'build',
       component: BuildView,
     },
-    {
-      path: '/output',
-      name: 'output',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/GuidedView.vue'),
-    },
+    // {
+    //   path: '/output',
+    //   name: 'output',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/GuidedView.vue'),
+    // },
   ],
 })
 
