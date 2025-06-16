109947487dedfe5cfbd2dd67316fccd6dbb29969
juronja <101410098+juronja@users.noreply.github.com>
2025-06-16 16:26:02 +0200

    fixed repo link

--- compose-commands.sh
+++ compose-commands.sh
@@ -5,8 +5,8 @@ docker image prune --all --force
 # docker system prune --all --force
 
 # Downloading and overwriting the compose file
-wget -O compose.base.yaml https://raw.githubusercontent.com/juronja/utm-builder/refs/heads/main/compose.base.yaml > compose.base.yaml
-wget -O compose.yaml https://raw.githubusercontent.com/juronja/utm-builder/refs/heads/main/compose.prod.yaml > compose.yaml
+wget -O compose.base.yaml https://raw.githubusercontent.com/juronja/icons-for-md/refs/heads/main/compose.base.yaml > compose.base.yaml
+wget -O compose.yaml https://raw.githubusercontent.com/juronja/icons-for-md/refs/heads/main/compose.prod.yaml > compose.yaml
 
 # Starting the container
 docker compose up -d --remove-orphans #--pull always
